acdf21cd2c5b5cb59e5cd99e30b789161a59e3db
5612466 <115354030+5612466@users.noreply.github.com>
2022-11-24 16:01:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int x=0, a=1, b=1, c=0;
+   cin >> x;
+   if (x<2)
+      cout <<"errore"<< endl;
+   else {
+      cout << a << endl;
+      cout << b << endl;
+   for (int i=0; i<=x; i++){
+      c=a+b;
+      a=b
+      b=c
+      cout << c << endl;
+      }
+   }
+   
    return 0;
 }
